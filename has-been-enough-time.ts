e8269373e4fbe803c31a653d094ca0e43c3da4da
Jackson McCluskey <25716756+jacksonmccluskey@users.noreply.github.com>
2024-07-02 16:20:40 -0700

    Enough Time Per Event

--- has-been-enough-time.ts
+++ has-been-enough-time.ts
@@ -1,33 +1,39 @@
 type Event = 'ONE' | 'TWO' | 'THREE';
 
 interface ITimeTracker {
+	enoughTime: number;
 	lastTime: Date | null;
 }
 
-const defaultTimeTracker: ITimeTracker = { lastTime: null };
+const defaultTimeTracker: ITimeTracker = { enoughTime: 0, lastTime: null };
 
 type TimeForEvent = {
 	[event in Event]: ITimeTracker;
 };
 
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
 const timeTracker: TimeForEvent = {
-	ONE: defaultTimeTracker,
-	TWO: defaultTimeTracker,
-	THREE: defaultTimeTracker
+	ONE: { ...defaultTimeTracker, enoughTime: day },
+	TWO: { ...defaultTimeTracker, enoughTime: hour },
+	THREE: { ...defaultTimeTracker, enoughTime: minute },
 };
 
-export const hasBeenEnoughTime = (enoughTime: number, event: Event) => {
+export const hasBeenEnoughTime = (event: Event) => {
 	const now = new Date();
 
 	const tracker = timeTracker[event];
 
-	if (tracker.lastTime == null || enoughTime == 0) {
+	if (tracker.lastTime == null || tracker.enoughTime == 0) {
 		tracker.lastTime = now;
 		return true;
 	}
 
 	const timeDifference =
-		now.getTime() - tracker.lastTime.getTime() > enoughTime;
+		now.getTime() - tracker.lastTime.getTime() > tracker.enoughTime;
 
 	if (!timeDifference) {
 		return false;
@@ -40,4 +46,16 @@ export const hasBeenEnoughTime = (enoughTime: number, event: Event) => {
 
 export const getLastTime = (event: Event) => {
 	return timeTracker[event].lastTime;
+};
+
+export const setLastTime = (event: Event, lastTime: Date | null) => {
+	timeTracker[event].lastTime = lastTime;
+};
+
+export const getEnoughTime = (event: Event) => {
+	return timeTracker[event].enoughTime;
+};
+
+export const setEnoughTime = (event: Event, enoughTime: number) => {
+	timeTracker[event].enoughTime = enoughTime;
 };
